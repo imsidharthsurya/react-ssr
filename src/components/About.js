65fadf07aa53d68978587673365c60efaa24d9c7
Sidharth Surya <imsidharthsurya@gmail.com>
2024-03-15 15:25:57 +0530

    latest

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='about'>
+        <h1>Sidharth Surya</h1>
+        <h2>Full Stack Developer</h2>
+        <h3>Hyderabad, Telangana</h3>
+    </div>
+  )
+}
+
+export default About
